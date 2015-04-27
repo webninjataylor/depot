dfdc4b21427bb8bab0d1c59e5e6a9643b735ca9f
webninjataylor <webninjataylor@gmail.com>
2015-04-27 17:06:56 -0400

    Fixture and new validation.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,6 +2,17 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
 
+  fixtures :products
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+      description: "yyy",
+      price: 1,
+      image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
